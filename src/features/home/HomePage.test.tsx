3378f06f6028e2bc447ddb92944df463fa9ba3a5
Barnabas Lesti <barnabas.lesti@gmail.com>
2026-04-21 16:49:50 +0200

    add vitest

---
+++ src/features/home/HomePage.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+
+import HomePage from "./HomePage";
+
+describe("HomePage", () => {
+  it("should render correctly", () => {
+    const { rerender } = render(<HomePage />);
+
+    rerender(<HomePage />);
+    expect(screen.getByRole("heading", { name: "Home Page" })).toBeInTheDocument();
+  });
+});
---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig, mergeConfig } from "vitest/config";
+
+import viteConfig from "./vite.config";
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    root: ".",
+    test: {
+      environment: "jsdom",
+      setupFiles: ["./vitest.setup.ts"],
+      coverage: {
+        provider: "v8",
+        reporter: ["text"],
+        include: ["src/**/*.{ts,tsx}"],
+      },
+    },
+  }),
+);
