efe20e341e253788513b6cdf608d897eaa18069e
Barnabas Lesti <barnabas.lesti@gmail.com>
2026-04-21 13:29:09 +0200

    tech: add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -17,5 +18,5 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
 });
